ce40c52e39c2293c32cfbda9968aacb7f70693a9
陳柏亨 <PoHeng.Chen@gmail.com>
2017-03-10 22:12:42 -0500

    1. finalized startup graph

--- project/03plotGraph_startup.R
+++ project/03plotGraph_startup.R
@@ -2,7 +2,6 @@
 ## Goal: to plot graph of top N investor/startup ##
 ####################################################
 ### select top n startup by its number of investment 
-N = 50
 ## plot top n startup graph
 d = data
 tmp = d %>% group_by(company_name) %>% summarize(num_investment = n())
@@ -31,32 +30,41 @@ g.degree = degree(g, v = V(g), mode = 'in',
                   loops = TRUE, normalized = FALSE)
 
 layout = layout.fruchterman.reingold(g)
-png('')
+png('top_startup_wo.png', width = 640, height = 640)
 plot(g, layout = layout, vertex.size = map(g.degree, c(1,5)),
      vertex.label = NA, vertex.color = V(g)$color,
      edge.arrow.size = .1)
-
-############################
-# work on bipartite, which means there should only be one edge
-############################
-# use in-degree as their importance
-# pr <- page.rank(g)$vector
-
-
-
-
-png('top_startup_wo.png', width = 640, height = 640)
-plot(g, layout = layout, vertex.size = 0.01, vertex.color = V(g)$color,#map(g.degree, c(1,100))/20 ,#map(pr, c(1,100))/20,
-     vertex.label = V(g)$investor_name,  vertex.label.font = 2, #vertex.label.dist=0.5,
-     # vertex.label.color = 'Black',
-     edge.arrow.size = .1, edge.color="#55555533")
-title('Top10 Investor Graph')
+title(sprintf('Top %s Investor Graph', N))
 dev.off()
 
-png('top_startup.png', width = 640, height = 640)
-plot(g, layout = layout, vertex.size = map(g.degree, c(1,100))/20 ,#map(pr, c(1,100))/20,
-     vertex.label = NA,
-     edge.arrow.size = .1, edge.color="#55555533")
-title('Top10 Investor Graph scaled by indegree')
+
+####################################################################
+### work on bipartite, which means there should only be one edge ###
+####################################################################
+
+d = d %>% group_by(investor_name, company_name) %>% summarize(num_investment = n())
+dt = d
+d = dt
+d = d[1:100,]
+
+# to create graph
+from = d$investor_name
+to = d$company_name
+all_edges = data.frame(from = from, to = to)
+g = graph.data.frame(all_edges, directed = F)
+
+# top n directed graph
+# create color 
+V(g)$type = bipartite_mapping(g)$type
+# assign size
+g.degree = degree(g, v = V(g), mode = 'in',
+                  loops = TRUE, normalized = FALSE)
+
+layout = layout.fruchterman.reingold(g)
+png('top_startup_single.png', width = 640, height = 640)
+plot(g, layout = layout, vertex.size = map(g.degree,c(1,5)),
+     vertex.label = NA, vertex.color = ifelse(V(g)$type,'blue','orange'),
+     edge.arrow.size = .1)
+title(sprintf('Top %s Investor Graph', N))
 dev.off()
 
